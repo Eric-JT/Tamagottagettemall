5c3b4896daf5e742d903ecb5b45b0bf570933e6f
Eric Trotchie <ejtrotchie@gmail.com>
2017-04-01 01:57:36 -0700

    Setting up the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tamagottagettemall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
